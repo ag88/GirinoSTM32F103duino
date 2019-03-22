d76617f5d773f5e37c7a0a4f2a0392f5232c4f13
Andrew Goh <gohandrew@yahoo.com>
2019-03-23 07:45:08 +0800

    accel girino compat v2 using DMA and AWD, draft
    added version 2 accelerated girino compat release draft
    rewrote much of codes, now girino stm32duino v2 is a significantly
    accelerated using stm32 hardware DMA and analog watchdog
    
    it now has no issues displaying signals the highest sample rates
    currently of Girinoscope 307.7 k samples per sec
    
    did further enhancement and achieved 500 k samples per sec - limit
    internally it is already doing a full 12 bits ADC buffers
    but 8 bits interfaced to Girinoscope for compatibility
    running on a single ADC
    
    this is still a draft release

---
+++ Girino_stm32f103mm_v2_compat/util_adc.h
@@ -0,0 +1,61 @@
+/*
+ *  this file comes from Arduino_STM32/STM32F1/libraries/STM32ADC/
+ *  name=STM32ADC
+ *  version=1.0
+ *  author=Cardoso, bubulindo, stevstrong, victorpv, roger ... many for libmaple adc codes
+ *  email=
+ *  sentence=Analog to Digital Converter Driver
+ *  paragraph=ADC for STM32F1
+ *  url=
+ *  architectures=STM32F1
+ *  maintainer=
+ *  category=Timing
+ */
+
+#ifndef __UTIL_ADC_H
+#define __UTIL_ADC_H
+
+#include <libmaple/adc.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+void start_single_convert(adc_dev* dev, uint8 channel);
+
+void start_continuous_convert(adc_dev* dev, uint8 channel);
+
+void enable_adc_irq(adc_dev* dev);
+
+void enable_internal_reading(adc_dev *dev);
+
+void internalRead(adc_dev *dev, uint8 channel);
+
+void enable_awd_irq( adc_dev * dev);
+
+void set_awd_low_limit( adc_dev * dev, uint32 limit);
+
+void set_awd_high_limit( adc_dev * dev, uint32 limit);
+
+void enable_awd( adc_dev * dev);
+
+void disable_awd( adc_dev * dev);
+
+void set_awd_channel( adc_dev * dev, uint8 awd_channel);
+
+void adc_set_reg_seq_channel( adc_dev * dev, unsigned char *channels, unsigned char len);
+
+void set_continuous( adc_dev * dev);
+
+uint8 poll_adc_convert(adc_dev *dev);
+
+void adc_dma_enable(adc_dev * dev);
+
+void adc_dma_disable(adc_dev * dev);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif //__UTIL_ADC_H
